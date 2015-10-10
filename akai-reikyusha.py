b5a83ff39e10cd75a016dafd0a626364d6403414
inokappa <inokara@gmail.com>
2015-10-10 22:25:01 +0900

    少し修正

--- akai-reikyusha.py
+++ akai-reikyusha.py
@@ -6,23 +6,21 @@ import ConfigParser
 import BeautifulSoup
 import twitter
 
-if __name__ == "__main__":
+# config.ini から Twitter API の Credential 情報を取得
+c = ConfigParser.SafeConfigParser()
+c.read("./config.ini")
 
-    # config.ini から Twitter API の Credential 情報を取得
-    c = ConfigParser.SafeConfigParser()
-    c.read("./config.ini")
-
-    # Twitter API を初期化
-    api = twitter.Api(
-        consumer_key        = c.get('tw','consumer_key'),
-        consumer_secret     = c.get('tw','consumer_secret'),
-        access_token_key    = c.get('tw','access_token_key'),
-        access_token_secret = c.get('tw','access_token_secret'),
-        )
-    
+# Twitter API を初期化
+api = twitter.Api(
+    consumer_key        = c.get('tw','consumer_key'),
+    consumer_secret     = c.get('tw','consumer_secret'),
+    access_token_key    = c.get('tw','access_token_key'),
+    access_token_secret = c.get('tw','access_token_secret'),
+    )
+ 
+def reikyusha():
     # wikipedia API から赤い霊柩車シリーズの情報を取得する(JSON 形式/コンテンツは HTML フォーマットで取得する)
     url = "https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=query&prop=revisions&titles=%E8%B5%A4%E3%81%84%E9%9C%8A%E6%9F%A9%E8%BB%8A%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA&rvprop=content&rvparse"
-
     # API より JSON データを取得
     htmldata = urllib2.urlopen(url)
     # JSON を解析
@@ -37,7 +35,10 @@ if __name__ == "__main__":
     for i in soup.findAll(text=re.compile(u"^\ 第.+」$")):
         # strip() 前後の空白を削除
     	episode.append(i.strip())
-	
+
+    return random.choice(episode)
+
+if __name__ == "__main__":
     # ランダムに作品名と放映日時を出力
-    print u"赤い霊柩車シリーズ: " + random.choice(episode)
-    print api.PostUpdate(u"赤い霊柩車シリーズ: " + random.choice(episode)) 
+    print u"赤い霊柩車シリーズ: " + reikyusha()
+    print api.PostUpdate(u"赤い霊柩車シリーズ: " + reikyusha()) 
