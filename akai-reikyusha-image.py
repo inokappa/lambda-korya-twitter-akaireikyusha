6fdf407c1ffc62b4c3608549641e926fb9bb7f22
inokappa <inokara@gmail.com>
2015-10-11 10:35:44 +0900

    画像投稿機能

--- akai-reikyusha-image.py
+++ akai-reikyusha-image.py
@@ -19,6 +19,35 @@ api = twitter.Api(
     access_token_secret = c.get('tw','access_token_secret'),
     )
 
+# 参考：http://qiita.com/tukiyo3/items/96724a3ae7c90d3ae387
+def get_img_urls(search_word, n):
+    img_url=[]
+    # 取得する画像 URL の数を指定
+    images_per_request=8
+    # https://developers.google.com/image-search/v1/jsondevguide
+    url = "http://ajax.googleapis.com/ajax/services/search/images?q=%e8%b5%a4%e3%81%84%e9%9c%8a%e6%9f%a9%e8%bb%8a&v=1.0&imgsz=huge&rsz="+str(images_per_request)+"&start={0}"
+    urlencoded = urllib2.quote(search_word)
+    # search_word と一致する画像 URL を 8 個取得
+    for i in range(n):
+    	res = urllib2.urlopen(url.format(i*images_per_request))
+    	data = json.load(res)
+    	img_url += [result["url"] for result in data["responseData"]["results"]]
+    # ランダムに 1 つの URL を返す
+    return random.choice(img_url)
+
+def download_img(url):
+    opener = urllib2.build_opener()
+    http = httplib2.Http(".cache")
+    response, content = http.request(url)
+    with open('image_file', 'wb') as f:
+    	f.write(content)
+    if os.path.exists('/tmp/image_file'):
+    	os.remove('/tmp/image_file')
+    shutil.move('image_file', '/tmp/')
+
+# 参考：http://qiita.com/yubais/items/864eedc8dccd7adaea5d
+# def upload_img():
+ 
 def reikyusha():
     # wikipedia API から赤い霊柩車シリーズの情報を取得する(JSON 形式/コンテンツは HTML フォーマットで取得する)
     url = "https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=query&prop=revisions&titles=%E8%B5%A4%E3%81%84%E9%9C%8A%E6%9F%A9%E8%BB%8A%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA&rvprop=content&rvparse"
@@ -40,7 +69,11 @@ def reikyusha():
     return random.choice(episode)
 
 if __name__ == "__main__":
+    # 赤い霊柩車シリーズの画像 URL を取得
+    image_url = get_img_urls("赤い霊柩車", 3)
+    print image_url
+    download_img(image_url)
     # ランダムに作品名と放映日時を出力
-    episode = reikyusha()
-    print u"赤い霊柩車シリーズ: " + episode
-    print api.PostUpdate(u"赤い霊柩車シリーズ: " + episode) 
+    # episode = reikyusha()
+    # print u"赤い霊柩車シリーズ: " + episode + image_url
+    # print api.PostUpdate(u"赤い霊柩車シリーズ: " + episode + image_url) 
